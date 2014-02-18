a491987d259d176427832be443b892aad10d33a8
Geroge <spartan322@Live.com>
2014-02-17 20:38:24 -0500

    Started API

---
+++ oppression/addons/addon-API.lua
@@ -0,0 +1,30 @@
+local addon = {}
+addon.amount = 1
+addon.new = {}
+
+
+
+function AddAddon(name)
+	addon.new[addon.amount+1] = name
+end
+
+function GetRoundTime()
+	return Round:GetTime()
+end
+
+function GetCurrentRound()
+	return Round:GetRound()
+end
+
+function GetAddons()
+	local files, directories = file.Find("gamemode/oppression/addons/*.lua", "GAME")
+	if directories then
+		for _,v in pairs(directories) do
+			AddAddon(directories[v])
+			local innerfiles, innerdirectories = file.Find("gamemode/oppression/addons/" .. directories[v], "GAME")
+			if innerfiles and innerdirectories then
+			include(directories[v] .. "/base/init"
+		end
+	end
+end
+				
