8ce6ac70884f05a1ccd859584614e744d0876d0f
Geroge <spartan322@Live.com>
2014-02-17 19:08:46 -0500

    Added Basic Console Commands

---
+++ oppression/gamemode/console.lua
@@ -0,0 +1,21 @@
+con = {}
+con.text = ""
+con.rtime = 30
+
+function con:AddText(text)
+	con.text += "[" .. con:GetRoundTime() .. "]" .. ": " .. text .. "\n"
+end
+
+function con:GetRoundTime()
+	return con.rtime
+end
+
+function con:PrintText()
+	print(con.text)
+end
+
+function con:EndText()
+	print("================= Round Begin =================\n")
+	print(con.text)
+	print("================= Round End ================="\n)
+end
