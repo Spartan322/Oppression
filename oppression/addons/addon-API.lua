85d611d052136c3cf902d611f969e5aea9e23647
Geroge <spartan322@Live.com>
2014-02-18 00:04:22 -0500

    Create Addon Checks

--- oppression/addons/addon-API.lua
+++ oppression/addons/addon-API.lua
@@ -1,11 +1,13 @@
 local addon = {}
 addon.amount = 1
 addon.new = {}
+addon.functions = {}
 
 
 
 function AddAddon(name)
 	addon.new[addon.amount+1] = name
+	addon.amount += 1
 end
 
 function GetRoundTime()
@@ -16,7 +18,30 @@ function GetCurrentRound()
 	return Round:GetRound()
 end
 
+function CreateAddonStart(name, func)
+	addon.functions[addon.amount+1] = func
+	AddAddon(name)
+end
+
+function RunAddonStart(addonID)
+	return addon.functions[addonID]
+end
+
+function GetAddonName(id)
+	return addon.new[id]
+end
+
+function GetAddonId(name)
+	for _,v in pairs(addon.new) do
+		if addon.new[v] == name
+			return v
+		end
+	end
+end
+
 function GetAddons()
+	print("Oppression Addon API started up.")
+	print("Getting Addons...")
 	local files, directories = file.Find("gamemode/oppression/addons/*.lua", "GAME")
 	if directories then
 		for _,v in pairs(directories) do
@@ -25,7 +50,9 @@ function GetAddons()
 			if innerfiles and innerdirectories then
 				include(directories[v] .. "/base/init")
 			end
+			RunAddonStarts(v)
 		end
 	end
+
 end
 				
