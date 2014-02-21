0f4e184fdf70b1ef6928d35dd5379c8493a85945
Geroge <spartan322@Live.com>
2014-02-21 08:40:33 -0500

    Update hud_base.lua

--- oppression/gamemode/vgui/hud_base.lua
+++ oppression/gamemode/vgui/hud_base.lua
@@ -1,8 +1,23 @@
+function hidehud(name) -- Removing the default HUD
+	for k, v in pairs({"CHudHealth", "CHudBattery", "CHudAmmo", "CHudSecondaryAmmo", })do
+		if name == v then return false end
+	end
+end
+hook.Add("HUDShouldDraw", "HideOurHud:D", hidehud)
+
 function DrawBaseHealthBar()
   	local ply = LocalPlayer()
   	local He = ply:Health()
   
-	surface.CreateFont( "ScoreboardText", 60, 400, false, false, "OPPbasefont" )
+	surface.CreateFont( 
+		"OPPbasefont",
+	{
+		font = "ScoreboardText",
+		size = 60,
+		weight = 400,
+		antialias = false,
+		shadow = false
+	})
   
   	draw.RoundedBox( 4, 120, ScrH() - 110, 300, 90, Color( 10, 10, 10, 255 ) )
   	draw.RoundedBox( 4, 130, ScrH() - 100, 200, 40, Color( 25, 30, 35, 80 ) )
