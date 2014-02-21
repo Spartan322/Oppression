08647111c146b2eb053ff2c390c6b0807409458c
Geroge <spartan322@Live.com>
2014-02-21 14:16:31 -0500

    Create scoreboard_base.lua

---
+++ oppression/gamemode/vgui/scoreboard_base.lua
@@ -0,0 +1,43 @@
+local PANEL = {}
+
+
+function PANEL:Init()
+	self.OverheadText = ""
+	self.OverheadFont = "ScoreboardText"
+	self.OverheadColor = Color(255,0,0,255)
+	self.OverheadTextAlign = TEXT_ALIGN_CENTER
+end
+
+function PANEL:Paint()
+  draw.RoundedBox(6,50,50,self:GetWide(),self:GetTall(),Color(0,0,0,0))
+  draw.DrawText( self.OverheadText, self.OverheadFont, x, 0, self.OverheadColor, self.OverheadTextAlign );
+  vgui.Create("sb_"
+ 
+  return true
+end
+
+-- This function is called whenever this control is pressed
+function PANEL:OnMousePressed()
+end
+
+function PANEL:SetOverheadText(text)
+	self.OverheadText = text
+end
+
+function PANEL:SetOverheadFont(font)
+	self.OverheadFont = font
+end
+
+function PANEL:SetOverheadColor(r,g,b,a)
+	if a == nil then
+		self.OverheadColor = Color(r,g,b,255)
+	else
+		self.OverheadColor = Color(r,g,b,a)
+	end
+end
+
+function PANEL:SetOverheadAlign(align)
+	self.OverheadTextAlign = align
+end
+
+vgui.Register( "OPPPanel", PANEL )
